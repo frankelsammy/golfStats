6cc6e10934d0aa75c8e00e2f9e202df9ffed59a6
frankelsammy <frankelsammy31@gmail.com>
2022-12-11 14:56:23 -0500

    auto

--- golfServer.js
+++ golfServer.js
@@ -10,7 +10,7 @@ process.stdin.setEncoding("utf8");
 const httpSuccessStatus = 200;
 
 app.set("views", path.resolve(__dirname, "templates"));
-var listener = app.listen(8888, function(){
+var listener = app.listen(4000, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 8888
 });
 app.set("view engine", "ejs");
